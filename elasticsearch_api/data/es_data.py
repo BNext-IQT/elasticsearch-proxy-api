8f14becbcbd657e38b4ae93f264ade9b21d821c9
David Mendez <dmendez@ebi.ac.uk>
2020-05-04 16:43:34 -0500

    Add first basic structure of the project

---
+++ elasticsearch_api/data/es_data.py
@@ -0,0 +1,9 @@
+"""
+Module that returns data from elascticsearch
+"""
+def get_es_response(index_name, raw_search_data):
+    """""
+    :param index_name: name of the index to query against
+    :param raw_search_data: stringification of the query to send to elasticsearch
+    """
+    return {}
---
+++ elasticsearch_api/data/test/test_es_data.py
@@ -0,0 +1,10 @@
+"""
+Module to test the data module
+"""
+import unittest
+
+class TestESData(unittest.TestCase):
+    """
+    Class to test the es data module
+    """
+    print('TEST')
