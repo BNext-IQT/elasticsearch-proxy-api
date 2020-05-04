0bc2b8e8c0c7c04522589d9bd0389981c774c6ac
David Mendez <dmendez@ebi.ac.uk>
2020-05-04 17:30:40 -0500

    Add unit tests to CI

---
+++ app/es_data/test/test_es_data.py
@@ -0,0 +1,15 @@
+"""
+Module to test the data module
+"""
+import unittest
+
+class TestESData(unittest.TestCase):
+    """
+    Class to test the es data module
+    """
+    def test_gets_es_data(self):
+        """
+        Tests that it returns the es data correctly
+        """
+        print('TEST')
+        self.assertEqual(0,0, msg='Mock test')
