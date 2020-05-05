2750b2d7e96e722b4b64e0551d33024bd286bd44
David Mendez <dmendez@ebi.ac.uk>
2020-05-05 14:41:58 -0500

    Add function for caching the response

--- app/es_data/es_data.py
+++ app/es_data/es_data.py
@@ -1,14 +1,34 @@
 """
 Module that returns data from elascticsearch
 """
+import json
+import hashlib
+import base64
+
 from app.es_connection import ES
 
 
-def get_es_response(index_name, search_data):
+def get_es_response(index_name, es_query):
     """""
     :param index_name: name of the index to query against
-    :param search_data: dict with the query to send
+    :param es_query: dict with the query to send
+    :return: the dict with the response from es
     """
 
-    response = ES.search(index=index_name, body=search_data)
+    response = ES.search(index=index_name, body=es_query)
     return response
+
+
+def get_es_query_cache_key(index_name, es_query):
+    """
+    Produces a key to save the data in the cache based on the parameters given
+    :param index_name: name of the index to query against
+    :param es_query: dict with the query to send
+    :return: the key to save the data in the cache
+    """
+
+    stable_raw_search_data = json.dumps(es_query, sort_keys=True)
+    search_data_digest = hashlib.sha256(stable_raw_search_data.encode('utf-8')).digest()
+    base64_search_data_hash = base64.b64encode(search_data_digest).decode('utf-8')
+
+    return f'{index_name}-{base64_search_data_hash}'
--- app/es_data/test/test_es_data.py
+++ app/es_data/test/test_es_data.py
@@ -2,6 +2,9 @@
 Module to test the data module
 """
 import unittest
+import json
+import hashlib
+import base64
 
 from app.es_data import es_data
 
@@ -43,3 +46,40 @@ class TestESData(unittest.TestCase):
         total_hits_got = response_got['hits']['total']
 
         self.assertEqual(total_hits_got, total_hits_must_be, msg='The response from elasticsearch was not correct')
+
+    def test_gets_cache_key(self):
+        """
+        Tests that it generates the correct cache key
+        """
+
+        es_query = {
+            'size': 24,
+            'from': 0,
+            'query': {
+                'bool': {
+                    'filter': [
+                        {
+                            'terms': {
+                                'molecule_chembl_id': [
+                                    'CHEMBL59',
+                                    'CHEMBL3247442',
+                                    'CHEMBL160074',
+                                    'CHEMBL2219748'
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        es_index = 'chembl_26_molecule'
+
+        stable_raw_search_data = json.dumps(es_query, sort_keys=True)
+        search_data_digest = hashlib.sha256(stable_raw_search_data.encode('utf-8')).digest()
+        base64_search_data_hash = base64.b64encode(search_data_digest).decode('utf-8')
+
+        cache_key_must_be = "{}-{}".format(es_index, base64_search_data_hash)
+        cache_key_got = es_data.get_es_query_cache_key(es_index, es_query)
+
+        self.assertEqual(cache_key_got, cache_key_must_be, msg='The cache key was not generated correctly!')
