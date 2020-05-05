246e962ce67b69ce1efd34740089c85d98bc10ca
David Mendez <dmendez@ebi.ac.uk>
2020-05-05 16:59:24 -0500

    Start to create module for saving es usage statistics

---
+++ app/usage_statistics/statistics_saver.py
@@ -0,0 +1,17 @@
+"""
+Module that saves statistics in elasticsearch
+"""
+def get_index_usage_record_dict(es_index, es_full_query, is_cached, request_date, run_env_type):
+    """
+    :param es_index: index used in the request
+    :param es_full_query: full query sent requested
+    :param is_cached: whether the data was cached or not
+    :param request_date: timestamp for the request date
+    :param run_env_type: type of run environment
+    :return: a dict to be used to save the cache statistics in the elasticsearch index
+    """
+
+    es_query = es_full_query.get('query', None)
+    es_aggs = es_full_query.get('aggs', None)
+
+    return {}
