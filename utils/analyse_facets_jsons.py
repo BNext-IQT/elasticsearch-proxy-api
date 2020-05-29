6e5b7ef2ac999aa29982b81939f2c7ce9b559be9
David Mendez <dmendez@ebi.ac.uk>
2020-05-29 17:22:28 -0500

    start to add script to analyse json final configs

--- utils/analyse_facets_jsons.py
+++ utils/analyse_facets_jsons.py
@@ -1,1 +1,21 @@
-print('HOLA')
+from os import listdir
+import json
+
+jsons_path = 'app/properties_configuration/config/temp'
+
+config_keys = set()
+
+for file_path in listdir(jsons_path):
+
+    print('going to read file: ', file_path)
+    with open(f'{jsons_path}/{file_path}', 'rt') as json_file:
+        facets_final_config = json.loads(json_file.read())
+
+        for item_key, config_value in facets_final_config.items():
+
+            print(set(config_value.keys()))
+            config_keys |= set(config_value.keys())
+
+
+print('config_keys: ')
+print(config_keys)
