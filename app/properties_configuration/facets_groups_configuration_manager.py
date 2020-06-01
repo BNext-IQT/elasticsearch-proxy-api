86e3f212569575375ac57368ef73d6699ec5a55a
David Mendez <dmendez@ebi.ac.uk>
2020-06-01 15:55:12 -0500

    Implement function to return facets config

---
+++ app/properties_configuration/facets_groups_configuration_manager.py
@@ -0,0 +1,74 @@
+"""
+    Module that handles the configuration of the groups of facets for the interface
+"""
+import os
+
+import yaml
+
+class FacetsGroupsConfiguration:
+    """
+    Class that handles the configuration of the groups of facets for the interface
+    """
+
+    class FacetsGroupsConfigurationManagerError(Exception):
+        """Base class for exceptions in the facets groups configuration."""
+
+    def __init__(self, facets_groups_file_path, property_configuration_manager):
+        """
+        :param facets_groups_file_path: path to the file where the facets groups config is
+        :param property_configuration_manager: instance of the properties configuration manager
+        """
+        if not os.path.isfile(facets_groups_file_path):
+            raise self.GroupsConfigurationManagerError(f'The path {path} does not exist!')
+
+        self.facets_groups_file_path = facets_groups_file_path
+        self.property_configuration_manager = property_configuration_manager
+
+    def get_facets_config_for_group(self, index_name, group_name):
+        """
+        :param index_name: name of the index to which the group belongs
+        :param group_name: name of the facets group
+        :return: the configuration for the facets group
+        """
+
+        with open(self.facets_groups_file_path, 'rt') as groups_file:
+
+            groups_config = yaml.load(groups_file, Loader=yaml.FullLoader)
+
+            index_groups = groups_config.get(index_name)
+            if index_groups is None:
+                raise self.FacetsGroupsConfigurationManagerError(
+                    f'The index {index_name} does not have a configuration set up!')
+
+            group_config = index_groups.get(group_name)
+            if group_config is None:
+                raise self.FacetsGroupsConfigurationManagerError(
+                    f'The group {group_name} does not exist in index {index_name}!')
+
+            default_properties = group_config['default']
+            optional_properties = group_config['optional']
+
+        return {
+            'properties': {
+                'default': self.get_facets_config_for_properties(default_properties, index_name),
+                'optional': self.get_facets_config_for_properties(optional_properties, index_name)
+            }
+        }
+
+    def get_facets_config_for_properties(self, props_dict, index_name):
+        """
+        :param props_dict: dictionary with the properties configuration from yaml
+        :param index_name: name of the index to which the group belongs
+        :return: the facets config for the properties in the dict
+        """
+        description = []
+        for prop_id, agg_config in props_dict.items():
+            prop_config = self.property_configuration_manager.get_config_for_prop(index_name, prop_id)
+            facet_config = {
+                'prop_id': prop_id,
+                'property_config': prop_config,
+                'agg_config': agg_config
+            }
+            description.append(facet_config)
+
+        return description
---
+++ app/properties_configuration/tests/test_facets_groups_configuration_manager.py
@@ -0,0 +1,92 @@
+"""
+Module to test groups configuration manager
+"""
+import unittest
+
+import yaml
+
+from app.properties_configuration.tests import test_property_configuration_manager
+from app.properties_configuration.facets_groups_configuration_manager import FacetsGroupsConfiguration
+from app.config import RUN_CONFIG
+
+
+def get_facets_groups_configuration_instance():
+    """
+    :return: a test instance of the facets configuration manager
+    """
+    configuration_manager = test_property_configuration_manager.get_property_configuration_instance()
+
+    facets_groups_configuration_manager = FacetsGroupsConfiguration(
+        facets_groups_file_path='app/properties_configuration/tests/data/test_facets_groups.yml',
+        property_configuration_manager=configuration_manager
+    )
+
+    return facets_groups_configuration_manager
+
+
+class FacetsGroupsConfigurationManagerTester(unittest.TestCase):
+    """
+    Class to test the facets configuration manager
+    """
+
+    def test_gets_config_for_a_group_fails_when_index_does_not_exist(self):
+        """
+        tests that getting config for a group fails when index does not exist
+        """
+
+        index_name = 'does_not_exist'
+        group_name = 'download'
+
+        facets_groups_configuration_manager = get_facets_groups_configuration_instance()
+
+        with self.assertRaises(FacetsGroupsConfiguration.FacetsGroupsConfigurationManagerError,
+                               msg='This should have thrown an exception for a non existing property!'):
+            facets_groups_configuration_manager.get_facets_config_for_group(index_name, group_name)
+
+    def test_gets_config_for_a_group_fails_when_group_does_not_exist(self):
+        """
+        tests that gets_config_for_a_group fails when group does not exist
+        """
+
+        es_index_prefix = RUN_CONFIG.get('es_index_prefix')
+        index_name = f'{es_index_prefix}molecule'
+        group_name = 'does_not_exist'
+
+        facets_groups_configuration_manager = get_facets_groups_configuration_instance()
+        with self.assertRaises(FacetsGroupsConfiguration.FacetsGroupsConfigurationManagerError,
+                               msg='This should have thrown an exception for a non existing property!'):
+            facets_groups_configuration_manager.get_facets_config_for_group(index_name, group_name)
+
+    def test_gets_config_for_a_facets_group(self):
+        """
+        tests that gets config for a group with default and additional properties
+        """
+        facets_groups_configuration_manager = get_facets_groups_configuration_instance()
+
+        es_index_prefix = RUN_CONFIG.get('es_index_prefix')
+        index_name = f'{es_index_prefix}molecule'
+        group_name = 'browser_facets'
+
+        configs_got = facets_groups_configuration_manager.get_facets_config_for_group(index_name, group_name)[
+            'properties']
+
+        with open(facets_groups_configuration_manager.facets_groups_file_path, 'rt') as groups_config_file:
+            groups_must_be = yaml.load(groups_config_file, Loader=yaml.FullLoader)
+            group_must_be = groups_must_be[index_name][group_name]
+
+            for type_key in ['default', 'optional']:
+                properties_got = configs_got[type_key]
+                properties_must_be = group_must_be[type_key]
+                self.assertEqual(len(properties_got), len(properties_must_be), msg=f'{type_key} configs are missing!')
+
+                for prop_desc in properties_got:
+                    prop_id = prop_desc['prop_id']
+                    agg_config_got = prop_desc['agg_config']
+                    agg_config_must_be = properties_must_be[prop_id]
+
+                    self.assertEqual(agg_config_got, agg_config_must_be,
+                                     msg='The agg config was not generated properly!')
+
+
+
+
