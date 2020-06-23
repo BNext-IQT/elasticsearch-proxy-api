60dd51aed290847b90562ba6cc8a30584f2652a6
David Mendez <dmendez@ebi.ac.uk>
2020-06-23 11:35:59 -0500

    ES Connection: Set up timout and retry_on_timeout from config

--- app/es_connection.py
+++ app/es_connection.py
@@ -7,5 +7,7 @@ from app.config import RUN_CONFIG
 ES = Elasticsearch(
     [RUN_CONFIG.get('elasticsearch').get('host')],
     http_auth=(RUN_CONFIG.get('elasticsearch').get('username'), RUN_CONFIG.get('elasticsearch').get('password')),
-    port=RUN_CONFIG.get('elasticsearch').get('port')
+    port=RUN_CONFIG.get('elasticsearch').get('port'),
+    timeout=RUN_CONFIG.get('elasticsearch').get('timeout', 10),
+    retry_on_timeout=RUN_CONFIG.get('elasticsearch').get('retry_on_timeout', False),
 )
