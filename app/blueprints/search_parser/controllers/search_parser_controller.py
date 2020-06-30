35bd0efc7ed769ff817ab2945bbb1ef632aa2962
David Mendez <dmendez@ebi.ac.uk>
2020-06-30 15:54:11 -0500

    Start to add endpoind to parse free text searches

---
+++ app/blueprints/search_parser/controllers/search_parser_controller.py
@@ -0,0 +1,14 @@
+"""
+Search parser controller
+"""
+from flask import Blueprint, jsonify, abort, request
+
+SEARCH_PARSER_BLUEPRINT = Blueprint('search_parsing', __name__)
+
+@SEARCH_PARSER_BLUEPRINT.route('/parse_free_text_search', methods=['POST'])
+def parse_free_text_search():
+    """
+    :return: the json response with the query to use in elasticsearch
+    """
+
+    return jsonify({'parsed_query': 'hola'})
