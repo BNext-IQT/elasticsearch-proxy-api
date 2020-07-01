4612605794cffe9cbf01572d1b0a9f40a11aadfe
David Mendez <dmendez@ebi.ac.uk>
2020-07-01 12:28:45 -0500

    Start to add service for free text parsing

--- app/blueprints/search_parser/controllers/search_parser_controller.py
+++ app/blueprints/search_parser/controllers/search_parser_controller.py
@@ -1,10 +1,12 @@
 """
 Search parser controller
 """
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, request
 
 from app.request_validation.decorators import validate_form_with
 from app.blueprints.search_parser.controllers import marshmallow_schemas
+from app.blueprints.search_parser.services import search_parsing_service
+from app.http_cache import http_cache_utils
 
 SEARCH_PARSER_BLUEPRINT = Blueprint('search_parsing', __name__)
 
@@ -16,4 +18,16 @@ def parse_free_text_search():
     :return: the json response with the query to use in elasticsearch
     """
 
-    return jsonify({'parsed_query': 'hola'})
+    form_data = request.form
+
+    search_term = form_data.get('search_term')
+    es_indexes = form_data.get('es_indexes')
+    selected_es_index = form_data.get('selected_es_index')
+
+    try:
+        raw_response = search_parsing_service.parse_search(search_term, es_indexes, selected_es_index)
+        http_response = jsonify(raw_response)
+        http_cache_utils.add_cache_headers_to_response(http_response)
+        return http_response
+    except search_parsing_service.SearchParsingServiceError as error:
+        abort(500, repr(error))
---
+++ app/blueprints/search_parser/services/search_parsing_service.py
@@ -0,0 +1,18 @@
+"""
+Search Parsing Service
+"""
+
+
+class SearchParsingServiceError(Exception):
+    """
+    Class for errors in this module
+    """
+
+def parse_search(search_term, es_indexes, selected_es_index):
+    """
+    :param search_term: Term to parse
+    :param es_indexes: indexes in which the search will be done, separated by a comma
+    :param selected_es_index: es index to focus on
+    :return: the query to send to elasticsearch based on the search term provided
+    """
+    return {'parsed_query': 'hola'}
